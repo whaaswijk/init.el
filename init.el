43cb7fc8c406baeda01813742444ce30f4eff5cc
Winston Haaswijk <winston.haaswijk@gmail.com>
2020-01-11 22:49:48 +0100

    Mostly configuration regarding C/C++ style

--- init.el
+++ init.el
@@ -1,3 +1,24 @@
 (org-babel-load-file "~/.emacs.d/configuration.org")
 (if (file-exists-p "~/.emacs.d/local-configuration.org")
     (org-babel-load-file "~/.emacs.d/local-configuration.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(blink-cursor-mode nil)
+ '(custom-enabled-themes (quote (wheatgrass)))
+ '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+    (git-auto-commit-mode org-bullets tabbar session pod-mode muttrc-mode mutt-alias markdown-mode initsplit htmlize graphviz-dot-mode folding eproject diminish csv-mode browse-kill-ring boxquote bm bar-cursor apache-mode)))
+ '(safe-local-variable-values (quote ((eval setq gac-automatically-push-p 1))))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
